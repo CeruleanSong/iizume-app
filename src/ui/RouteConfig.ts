a31d29969a7f314f6a8da3071aa59275f4feb235
Elias Mawa <elias@emawa.io>
2019-12-31 13:49:19 -0700

    clean up whitespace error

--- src/ui/RouteConfig.ts
+++ src/ui/RouteConfig.ts
@@ -7,51 +7,51 @@
  * @author Elias Mawa <elias@emawa.io>
  */
 
- import Browse from './Pages/Browse';
- import BrowseTabNavigator from './Pages/BrowseTabNavigator';
- import Gallery from './Pages/Gallery';
- import Library from "./Pages/Library";
- import Settings from "./Pages/Settings";
- 
- const Routes = {
-	 Library: {
-		 name: "Library",
-		 screen: Library,
-	 },
-	 Latest: {
-		 name: "Latest",
-		 screen: Browse,
-	 },
-	 Browse: {
-		 name: "Browse",
-		 screen: Browse,
-	 },
-	 BrowseTabNavigator: {
-		 name: 'BrowseTabNavigator',
-		 screen: BrowseTabNavigator,
-		 Routes: {
-			 Search: {
-				 name: "Latest",
-				 // screen: Browse,
-			 },
-			 Latest: {
-				 name: "Latest",
-				 // screen: Browse,
-			 },
-			 Hot: {
-				 name: "Latest",
-			 // screen: Browse,
-			 },
-		 },
-	 },
-	 Gallery: { /* NOTE: Might not be needed ? */
-		 name: "Gallery",
-		 screen: Gallery,
-	 },
-	 Settings: {
-		 name: "Settings",
-		 screen: Settings,
-	 },
- };
- 
- export default Routes;
+import Browse from './Pages/Browse';
+import BrowseTabNavigator from './Pages/BrowseTabNavigator';
+import Gallery from './Pages/Gallery';
+import Library from "./Pages/Library";
+import Settings from "./Pages/Settings";
+
+const Routes = {
+	Library: {
+		name: "Library",
+		screen: Library,
+	},
+	Latest: {
+		name: "Latest",
+		screen: Browse,
+	},
+	Browse: {
+		name: "Browse",
+		screen: Browse,
+	},
+	BrowseTabNavigator: {
+		name: 'BrowseTabNavigator',
+		screen: BrowseTabNavigator,
+		Routes: {
+			Search: {
+				name: "Latest",
+				// screen: Browse,
+			},
+			Latest: {
+				name: "Latest",
+				// screen: Browse,
+			},
+			Hot: {
+				name: "Latest",
+			// screen: Browse,
+			},
+		},
+	},
+	Gallery: { /* NOTE: Might not be needed ? */
+		name: "Gallery",
+		screen: Gallery,
+	},
+	Settings: {
+		name: "Settings",
+		screen: Settings,
+	},
+};
+
+export default Routes;
