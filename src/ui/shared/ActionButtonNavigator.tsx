398c08e126fdef52ebfb57f0869fc55dba9dbee4
Filip Ekström <Filip.ekstrom98@gmail.com>
2020-01-01 18:15:16 +0100

    Slight update to action button to get nicer shadows

--- src/ui/shared/ActionButtonNavigator.tsx
+++ src/ui/shared/ActionButtonNavigator.tsx
@@ -19,52 +19,77 @@ interface ActionButtonNavigatorProps {
 }
 
 // tslint:disable-next-line: no-empty-interface
-interface ActionButtonNavigatorState { }
+interface ActionButtonNavigatorState {
+	active: boolean;
+}
 
 /**
  * Component for saved manga
  */
-class ActionButtonNavigator extends React.Component<ActionButtonNavigatorProps> {
+class ActionButtonNavigator extends React.Component<ActionButtonNavigatorProps, ActionButtonNavigatorState> {
 
 	constructor(props: ActionButtonNavigatorProps){
 		super(props);
-		this.state = {};
+		this.state = {
+			active: false,
+		};
+
+		this.toggleState = this.toggleState.bind(this);
+		this.handlePress = this.handlePress.bind(this);
 	}
 
 	public render(){
 		return (
-			<ActionButton buttonColor="#ffb7c5" size={largeButtonRadius}
-				renderIcon={(active) => (active) ?
-					(<Icon name="expand-less" style={styles.actionButtonIcon}/>) :
-					(<Icon name="expand-less" style={styles.actionButtonIcon}/>)}
+			<ActionButton
+				active={this.state.active}
+				hideShadow={!this.state.active}
+				onPress={() => this.handlePress()}
+				buttonColor="#ffb7c5"
+				size={largeButtonRadius}
+				renderIcon={() => <Icon name="expand-less" style={styles.actionButtonIcon}/>}
 				degrees={rotation}>
 
-				<ActionButton.Item buttonColor='#e05c74' title="Library" size={smallButtonRadius}
+				<ActionButton.Item
+					buttonColor='#e05c74' title="Library" size={smallButtonRadius}
 					onPress={() => this.props.navigation.navigate(AppRoutes.Library.name)}>
 					<Icon name="book" style={styles.actionButtonIcon} />
 				</ActionButton.Item>
 
-				<ActionButton.Item buttonColor='#e05c74' title="Updates" size={smallButtonRadius}
+				<ActionButton.Item
+					buttonColor='#e05c74' title="Updates" size={smallButtonRadius}
 					onPress={() => this.props.navigation.navigate(AppRoutes.Updates.name)}>
 					<Icon name="new-releases" style={styles.actionButtonIcon} />
 				</ActionButton.Item>
 
-				<ActionButton.Item buttonColor='#e05c74' title="History" size={smallButtonRadius}
+				<ActionButton.Item
+					buttonColor='#e05c74' title="History" size={smallButtonRadius}
 					onPress={() => this.props.navigation.navigate(AppRoutes.History.name)}>
 					<Icon name="history" style={styles.actionButtonIcon} />
 				</ActionButton.Item>
 
-				<ActionButton.Item buttonColor='#e05c74' title='Browse' size={smallButtonRadius}
+				<ActionButton.Item
+					buttonColor='#e05c74' title='Browse' size={smallButtonRadius}
 					onPress={() => this.props.navigation.navigate(`${AppRoutes.Browse.name}`)}>
 					<Icon name="search" style={styles.actionButtonIcon} />
 				</ActionButton.Item>
 
-				<ActionButton.Item buttonColor='#e05c74' title="Settings" size={smallButtonRadius}
+				<ActionButton.Item
+					buttonColor='#e05c74' title="Settings" size={smallButtonRadius}
 					onPress={() => this.props.navigation.navigate(AppRoutes.Settings.name)}>
 					<Icon name="settings" style={styles.actionButtonIcon} />
 				</ActionButton.Item>
 			</ActionButton>
 		);
+	}
+
+	private handlePress(){
+		this.toggleState();
+	}
+
+	private toggleState(){
+		this.setState({
+			active: !this.state.active,
+		});
 	}
 }
 
