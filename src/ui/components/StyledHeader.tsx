e613e10055d5c31af6198f4ccca5872da13ef64c
Filip Ekström <Filip.ekstrom98@gmail.com>
2020-01-02 03:00:53 +0100

    Add back button implementation for header

--- src/ui/components/StyledHeader.tsx
+++ src/ui/components/StyledHeader.tsx
@@ -9,18 +9,42 @@
 
 import React from "react";
 import { StyleSheet, Text, TextStyle, View, ViewStyle } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { withNavigation } from "react-navigation";
+import { NavigationStackProp } from "react-navigation-stack";
 
+/**
+ * 
+ */
 interface HeaderProps {
 	style?: ViewStyle;
 	textStyle?: TextStyle;
 	children?: Element; // TODO: Fix type. Can't remember what type this is LOL
+	showBackButton?: boolean;
+	onBackButtonPressed?: () => void;
 	text: string;
+
+	navigation: NavigationStackProp;
 }
 
 const StyledHeader = (props: HeaderProps) => {
+	const backButtonPressed = () => {
+		if (props.onBackButtonPressed){
+			props.onBackButtonPressed();
+		}
+		else{
+			props.navigation.goBack(); // Default to navigation back if no onBackButton callback is specified.
+		}
+	};
+
 	return (
 		<View style={style.header}>
-			{props.children}
+			{(props.showBackButton ?? true) && // Default to showing back button if not passed into props
+				<TouchableOpacity activeOpacity={0.8} style={style.backButton} onPress={() => backButtonPressed()}>
+					<Icon name="arrow-back" color="#ffffff" style={{fontSize: 22}}></Icon>
+				</TouchableOpacity>
+			}
 			<Text style={style.headerText}>{props.text}</Text>
 		</View>
 	);
@@ -28,13 +52,12 @@ const StyledHeader = (props: HeaderProps) => {
 
 const style = StyleSheet.create({
 	header: {
-		// minHeight: "5%",
-		// minHeight: "5%",
 		width: "100%",
 
 		backgroundColor: "#e05c74",
 
-		justifyContent: "center",
+		flexDirection: "row",
+		justifyContent: "flex-start",
 		alignItems: "center",
 		padding: 8,
 	},
@@ -46,6 +69,19 @@ const style = StyleSheet.create({
 		color: "#ffffff",
 		margin: 8,
 	},
+	backButton: {
+		borderRadius: 1000,
+		overflow: "hidden",
+
+		height: 40,
+		width: 40,
+
+		justifyContent: "center",
+		alignItems: "center",
+
+		marginRight: 20,
+		marginLeft: 10,
+	},
 });
 
-export default StyledHeader;
+export default withNavigation(StyledHeader);
